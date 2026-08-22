a221e2dd2bafc41c0c4b9c5fde688bdd2403fcf9
Whandryn <whandryn@protonmai.com>
2026-08-22 09:12:06 +0100

    remove rounded corners

--- src/components/NoteListRow.tsx
+++ src/components/NoteListRow.tsx
@@ -263,14 +263,12 @@ const NoteListRow = ({
         display: "flex",
         alignItems: "center",
         borderBottom: isPriorityBoundary ? "6px solid " : "3px solid",
-        borderTopLeftRadius:
-          isPriorityGroupStart || isNonPriorityGroupStart ? 8 : 0,
-        borderTopRightRadius:
-          isPriorityGroupStart || isNonPriorityGroupStart ? 8 : 0,
+        borderTopLeftRadius: isPriorityGroupStart ? 8 : 0,
+        borderTopRightRadius: isPriorityGroupStart ? 8 : 0,
         borderBottomLeftRadius:
-          isPriorityGroupEnd || isNonPriorityGroupEnd || isLastNote ? 12 : 0,
+          isPriorityGroupEnd || isLastNote ? 12 : 0,
         borderBottomRightRadius:
-          isPriorityGroupEnd || isNonPriorityGroupEnd || isLastNote ? 12 : 0,
+          isPriorityGroupEnd || isLastNote ? 12 : 0,
         borderColor: colors.grey[900],
         overflow: "hidden",
         touchAction: "pan-y",
